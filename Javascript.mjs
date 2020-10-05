23e0b26d81c1d20c5506dc0cb29023645f5dea68
fiona-yelland <64481575+fiona-yelland@users.noreply.github.com>
2020-10-05 22:11:34 +0100

    nav

--- Javascript.mjs
+++ Javascript.mjs
@@ -1,16 +1,17 @@
-var mywindow = $(window);
-var mypos = mywindow.scrollTop();
-var up = false;
-var newscroll;
-mywindow.scroll(function () {
-    newscroll = mywindow.scrollTop();
-    if (newscroll > mypos && !up) {
-        $('.navigationBar').stop().fadeOut();
-        up = !up;
-        console.log(up);
-    } else if(newscroll < mypos && up) {
-        $('.navigationBar').stop().fadeIn();
-        up = !up;
-    }
-    mypos = newscroll;
-});
+// add padding top to show content behind navbar
+$('body').css('padding-top', $('.navbar').outerHeight() + 'px')
+
+// detect scroll top or down
+if ($('.smart-scroll').length > 0) { // check if element exists
+    var last_scroll_top = 0;
+    $(window).on('scroll', function() {
+        scroll_top = $(this).scrollTop();
+        if(scroll_top < last_scroll_top) {
+            $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+        }
+        else {
+            $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
+        }
+        last_scroll_top = scroll_top;
+    });
+}
